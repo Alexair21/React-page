cdbebe738c391e022d94dfd80145c30cb3ae0aa9
Diego Huamanjulca <diegohuamnjulcaguerrero2002@gmail.com>
2024-08-18 22:14:46 -0500

    refactor: Update Footer component layout and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import "./Footer.css";
 import logo from '../assets/logo.png';
 
 const Footer = () => {
   return (
-    <div className="footer ">
-      <div className="borde relative ">
-        <div className="overlay"></div>
-        <img className="logo" src={logo} alt="" />
-        <p className="footer-text -mt-3">© La Gran Concha Norteña 2024. Todos los derechos reservados.</p>
+    <div className="footer text-center">
+      <div className="borde relative mt-4 w-full h-64 overflow-hidden">
+        <div className="overlay absolute inset-0 bg-primary bg-opacity-50"></div>
+        <img className="relative z-10 w-[50%] mx-auto md:w-[15%]" src={logo} alt="Logo" />
+        <p className="relative z-10 text-white text-sm lg:text-base md:-mt-4 mt-0">
+          © La Gran Concha Norteña 2024. Todos los derechos reservados.
+        </p>
       </div>
-      <div className="creador bg-primary text-white p-2">
-        <p>Desarrollado por<span className="text-secondary"> Alexair21</span></p>
+      <div className="bg-primary text-white py-2">
+        <p>
+          Desarrollado por <span className="text-secondary">Alexair21</span>
+        </p>
       </div>
     </div>
   );
